16a3c1996b24bc0ae94e260b37e1342bfa58a862
Haiqal Halim <haiqal.halim@daysmart.com>
2023-08-07 01:43:12 +0800

    add music, adjust js file to auto play, change main digital

--- js/card-main.js
+++ js/card-main.js
@@ -8,6 +8,7 @@ $(function(){
         $('body').find('.opening-card').addClass('d-none');
         $('body').find('.main-div').removeClass('d-none');
         $('body').find('.navbar-card').removeClass('d-none');
+        $('body').find('#music-btn').trigger('click');
         AOS.refresh();  // ---  need to reinitiate after unhide
     }, 750);
    })
